b48ea33d612a0d5cce3c8fe57490f9f300acf4b0
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-03-16 16:31:19 +0530

    Explicit animation completed

--- lib/screen/categories_screen.dart
+++ lib/screen/categories_screen.dart
@@ -86,9 +86,15 @@ class _CategoryScreenState extends State<CategoryScreen>
             )
         ],
       ),
-      builder: (context, child) => Padding(
-        padding: EdgeInsets.only(
-          top: 100 - _animationController.value * 100,
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
         ),
         child: child,
       ),
